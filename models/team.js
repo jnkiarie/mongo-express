20f727c8dfbfce5de81d92aa857ae65390dba7c8
jnkiarie <92355202+jnkiarie@users.noreply.github.com>
2022-02-28 22:40:30 +0300

    delete schema for testing purposes

--- models/team.js
+++ models/team.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-//Define a schema
-const teamSchema = new Schema({
-    'teamName': String,
-    'stadiumName': String,
-    'teamSize': Number
-})
-// Create a model from the schema
-const Team = mongoose.model('Team', teamSchema);
+// const Schema = mongoose.Schema;
+// //Define a schema
+// const teamSchema = new Schema({
+//     'teamName': String,
+//     'stadiumName': String,
+//     'teamSize': Number
+// })
+// // Create a model from the schema
+// const Team = mongoose.model('Team', teamSchema);
 
-// Export it
-module.exports = Team;
+// // Export it
+// module.exports = Team;
 
